890d872ce4a5dbe7d60da6d557a33174532d5760
jeffvos <jeff.vos@aol.com>
2022-10-20 11:22:33 +0200

    adding submit button to form

--- dash/src/scenes/form/index.jsx
+++ dash/src/scenes/form/index.jsx
@@ -145,6 +145,11 @@ const Form = () => {
                 }}
               />
             </Box>
+            <Box display="flex" justifyContent="end" mt="20px">
+              <Button type="submit" color="secondary" variant="contained">
+                Create New User
+              </Button>
+            </Box>
           </form>
         )}
       </Formik>
