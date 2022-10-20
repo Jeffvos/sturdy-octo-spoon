3d687e9feb687e410fbe34b9020b0797cc4e2e79
jeffvos <jeff.vos@aol.com>
2022-10-20 13:43:36 +0200

    finishes calendar

--- dash/src/scenes/calendar/index.jsx
+++ dash/src/scenes/calendar/index.jsx
@@ -81,6 +81,34 @@ const Calendar = () => {
             ))}
           </List>
         </Box>
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next,today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              { id: "1234", title: "Allday event", date: "2022-10-20" },
+              { id: "4312", title: "Timed event", date: "2022-10-21" },
+            ]}
+          />
+        </Box>
       </Box>
     </Box>
   );
