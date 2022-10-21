f6dda39a871670185f5c605ebcd1f555a3d76919
jeffvos <jeff.vos@aol.com>
2022-10-21 07:07:02 +0200

    progress dashboard

--- dash/src/scenes/dashboard/index.jsx
+++ dash/src/scenes/dashboard/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, IconButton, useTheme } from "@mui/material";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
 import { mockTransactions } from "../../data/mockData";
@@ -119,6 +119,42 @@ const Dashboard = () => {
               />
             }
           />
+        </Box>
+
+        <Box
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Typography variant="h5" fontWeight="600" color={colors.grey[100]}>
+              Revenue Generated
+            </Typography>
+            <Typography
+              variant="h3"
+              fontWeight="500"
+              color={colors.greenAccent[500]}
+            >
+              $1,023,123,23
+            </Typography>
+
+            <Box>
+              <IconButton>
+                <FileDownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                />
+              </IconButton>
+            </Box>
+          </Box>
+          <Box height="250px" ml="-20px 0 0 0">
+            <LineChart isDashboard={true} />
+          </Box>
         </Box>
       </Box>
     </Box>
